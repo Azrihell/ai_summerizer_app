2946caabc655e92f18d0407b2c5a2be4125ff1d3
Azriel <azrihell.l@gmail.com>
2023-09-11 18:10:44 -0500

    added enter button

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,22 +4,27 @@ import { copy, linkIcon, loader, tick } from "../assets"
 const Demo = () => {
   return (
     <section className="mt-16 w-full max-w-xl">
-    {/* search */}
+      {/* search */}
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" 
-        onSubmit={() => {}}>
-        <img 
-          src={linkIcon}
-          alt="link_icon"
-          className="absolute left-0 my-2 ml-3 w-5"/>
-          <input 
+        <form className="relative flex justify-center items-center"
+          onSubmit={() => { }}>
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5" />
+          <input
             type="url"
             placeholder="Enter your URL"
             value=""
-            onChange={() => {}}
+            onChange={() => { }}
             required
-            className="url_input peer"
-          />
+            className="url_input peer" />
+
+          <button
+            type="submit"
+            className="submit_btn">
+            ↵
+          </button>
         </form>
       </div>
     </section>
