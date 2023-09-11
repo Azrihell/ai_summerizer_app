a7aea6bff299d8b3a28d9caf59b128a25a062237
Azriel <azrihell.l@gmail.com>
2023-09-11 18:05:03 -0500

    added link input box

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,7 +8,18 @@ const Demo = () => {
       <div className="flex flex-col w-full gap-2">
         <form className="relative flex justify-center items-center" 
         onSubmit={() => {}}>
-
+        <img 
+          src={linkIcon}
+          alt="link_icon"
+          className="absolute left-0 my-2 ml-3 w-5"/>
+          <input 
+            type="url"
+            placeholder="Enter your URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
         </form>
       </div>
     </section>
