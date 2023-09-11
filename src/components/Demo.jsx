527a5ea8b72ea64ec6ad3970313b589736b6bfe6
Azriel <azrihell.l@gmail.com>
2023-09-11 18:20:55 -0500

    submitted working with urls

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,21 +2,33 @@ import { useState, useEffect } from "react"
 import { copy, linkIcon, loader, tick } from "../assets"
 
 const Demo = () => {
+
+  const [article, setArticle] = useState({
+    url: '',
+    summary: ''
+  })
+
+  const handleSubmit = async (e) => {
+    alert('Submitted')
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* search */}
       <div className="flex flex-col w-full gap-2">
         <form className="relative flex justify-center items-center"
-          onSubmit={() => { }}>
+          onSubmit={handleSubmit}>
+
           <img
             src={linkIcon}
             alt="link_icon"
             className="absolute left-0 my-2 ml-3 w-5" />
+
           <input
             type="url"
             placeholder="Enter your URL"
-            value=""
-            onChange={() => { }}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer" />
 
